eb4a2f96969f4496ea4213ab9f62dc2ae92c95d0
ViacheslavHaidai <145702643+ViacheslavHaidai@users.noreply.github.com>
2023-12-13 10:13:36 +0200

    1

--- js/training.js
+++ js/training.js
@@ -1,9 +1,33 @@
-const start = 6;
-const end = 17;
-let number;
-for(let i = start; start < i < end; i++){
-    console.log(i);
-    if(number / 5){
-        return number;
+
+// function calculateEvenTotal(number) {
+//     let total = 0;
+//     for (let i = 2; i <= number; i += 2) {
+//       total += i;
+//     }
+//     return total;
+//   }
+  
+//   console.log(calculateEvenTotal(6))
+
+
+
+function divide(number){
+    if(number % 5 === 0){
+        return number
+    } else {
+        return 'you are loh'
     }
 }
+console.log(divide(15))
+
+
+// let total = 0;
+// for (let i = 1; i <= 5; i++) {
+//     console.log(total += i);
+//   }
+
+// const end = 29
+// let number;
+//     for (let start = 6; start < end; start++) {
+//       console.log(start);
+//     }
