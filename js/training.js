667443ae4a81c8fc0bbb2e449478d31169a1e56b
ViacheslavHaidai <145702643+ViacheslavHaidai@users.noreply.github.com>
2023-12-16 17:15:59 +0200

    1

--- js/training.js
+++ js/training.js
@@ -41,13 +41,64 @@
 // console.log(checkString(7))
 
 //Напишіть цикл (for), який виведе в консоль усі парні числа від max до min включно по зменшенню
-const max = 50;
-const min = 23;
+// const max = 50;
+// const min = 23;
 
-for (let i = max; i < min; i -= 2) {
-    if (i % 2 !== 0){
-        continue
-    } else {
-        console.log(i)
-    }
+// for (let i = max; i >= min; i--) {
+//     if (i % 2 !== 0){
+//         continue
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+
+
+// За допомогою циклу for знайдіть суму усіх парних чисел у проміжку від min до max включно
+// const max = 50;
+// const min = 0;
+// let total = 0;
+// for (let i = min; i <= max; i++) {
+//     if (i % 2 !== 0){
+//         continue
+//     } else {
+//         total += i 
+        
+//     }
+
+// }
+
+// console.log(total)
+
+
+
+
+// Напишіть програму, яка обчислить суму всіх чисел (за допомогою циклу for)
+// від 1 до 100, які діляться на 3 або на 5. І виведи в консоль рядок :
+// `Сума чисел, які діляться на 3 або на 5 від 1 до 100: ${sum}`
+// let total = 0
+// for (let i = 1; i <= 100; i++){
+//     if (i % 3 === 0 || i % 5 === 0) {
+//         total += i
+//     }
+// }
+// console.log(`Сума чисел, які діляться на 3 або на 5 від 1 до 100: ${total}`)
+
+
+
+
+// Напишіть функцію formatMinutesToTime(minutes), котра
+// отримає від користувача число(кількість хвилин) і виведе
+// в консоль рядок у форматі годин і хвилин, тобто, якщо користувач
+// вказав число 70, в консолі отримаємо "01:10"
+
+function formatMinutesToTime(minutes){
+    const hours = Math.floor(minutes / 60 )
+    const modHours = String(hours).padStart(2, '0')
+    const modMinutes = String(minutes % 60).padStart(2, '0')
+
+    
+    return `${modHours} : ${modMinutes}`
 }
+
+console.log(formatMinutesToTime(200))
